776324f786e5dac02261e7bee41b17acb6d36c4d
darkchoco <darkchoco@cloudscape.kr>
2023-02-07 14:08:58 +0100

    [aiml-coders] .gitignore, first.py updated.

--- ai-ml-coders/first.py
+++ ai-ml-coders/first.py
@@ -2,6 +2,9 @@ import numpy as np
 from keras import Sequential
 from keras.layers import Dense
 
+# Sequential class로 layer를 정의. Dense는 그 layer의 형태가 어떤 것인지를 의미한다.
+# Dense => 뉴런이 완전히(또는 조밀하게) 연결되어 있다는 것.
+# units=1 => 이 layer는 한개의 뉴런을 가진다.
 model = Sequential([Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
